16e47a2af7745f4a062152a32d7e3350b9fccd9d
2Fool <guanyh23@163.com>
2020-06-02 18:35:07 +0800

    rename

--- src/main/java/com/zzh/algorithm/AlgorithmApplication.java
+++ src/main/java/com/zzh/algorithm/AlgorithmApplication.java
@@ -1,4 +1,4 @@
-package com.zzh.Algorithm;
+package com.zzh.algorithm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/zzh/algorithm/AlgorithmApplicationTests.java
+++ src/test/java/com/zzh/algorithm/AlgorithmApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zzh.Algorithm;
+package com.zzh.algorithm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
