346c5c9f8267cbc294229d7b66dbe9cf4eff241c
2Fool <guanyh23@163.com>
2020-06-02 18:08:28 +0800

    init

---
+++ src/main/java/com/zzh/demo/DfaApplication.java
@@ -0,0 +1,13 @@
+package com.zzh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfaApplication.class, args);
+	}
+
+}
