ac63154edd9adec20a12a57f2889300deefe1a2c
yuhui.guan <yuhui.guan@speedship.cn>
2020-07-29 19:38:28 +0800

    添加注释

--- src/main/java/com/zzh/algorithm/encryption/Base64Test.java
+++ src/main/java/com/zzh/algorithm/encryption/Base64Test.java
@@ -7,6 +7,7 @@ import java.util.Base64;
  * new String(Base64.encodeBase64(bytes[])) = java.util.Base64.getEncoder().encodeToString((bytes[]))
  * new String(Base64.decodeBase64(bytes[])) = java.util.Base64.getDecoder().decode(bytes)
  * java8 自带拥有了base64 https://blog.csdn.net/iamlihongwei/article/details/97793114
+ * encryption的来源 https://mp.weixin.qq.com/s/AtbFyW6yO09nPT3aYV4rdg
  *
  * @Author: yuhui.guan
  * @Date: 2020/7/29 17:30
